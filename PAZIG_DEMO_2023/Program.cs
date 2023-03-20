7dbbee83a071b6a6e81b3f3e5d1e63189ef52be0
Agnieszka Różańska <agniroz767@student.polsl.pl>
2023-03-20 16:11:22 +0100

    Create empty project

---
+++ PAZIG_DEMO_2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PAZIG_DEMO_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
