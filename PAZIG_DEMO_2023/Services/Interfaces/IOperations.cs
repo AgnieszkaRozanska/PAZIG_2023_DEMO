6ea1c903ca9d8340429d34c6e9abec2a10f89ace
Agnieszka Różańska <agniroz767@student.polsl.pl>
2023-03-20 16:19:42 +0100

    Implementation Add function

---
+++ PAZIG_DEMO_2023/Services/Interfaces/IOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAZIG_DEMO_2023.Services.Interfaces
+{
+    public interface IOperations
+    {
+        double Add(double firstNumber, double secondNumber);
+    }
+}
---
+++ PAZIG_DEMO_2023/Services/Operations.cs
@@ -0,0 +1,17 @@
+﻿using PAZIG_DEMO_2023.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAZIG_DEMO_2023.Services
+{
+    public class Operations : IOperations
+    {
+        public double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
